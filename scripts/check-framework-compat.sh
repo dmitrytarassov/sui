99892190ee9ca137e666d7586eda70dc93b1ef48
Ashok Menon <ashok@mystenlabs.com>
2023-05-01 16:23:46 -0700

    [move-cli] Update framework branch in sui move new template
    Differentiate the branch that node operators build their software off
    of (e.g. `mainnet`) from the branch that Move package developers
    depend on the framework at (e.g. `framework/mainnet`).
    
    This is to account for the fact that when a protocol version/binary
    upgrade includes a framework change, there is a period of time between
    when the tracking branch for node operators (e.g. `mainnet`) is
    updated and when the network performs the protocol upgrade where an
    attempt to publish a package that depends on a system package will
    fail because of dependency source validation.
    
    This PR also introduces some other minor changes to the new package
    template:
    
    - Remove an extraneous space in the `addresses` output.
    - Don't include the address for `sui` because it will be inherited
      from the `Sui` dependency automatically.
    
    Test Plan:
    
    Test output from `sui move new`
    
    ```
    crates/sui$ cargo run -- --path /tmp/example example
    crates/sui$ cat /tmp/example/Move.toml
    [package]
    name = "example"
    version = "0.0.1"
    
    [dependencies]
    Sui = { git = "https://github.com/MystenLabs/sui.git", subdir = "crates/sui-framework/packages/sui-framework", rev = "framework/testnet" }
    
    [addresses]
    example = "0x0"
    ```
    
    Test script that checks compatibility against localnet:
    
    ```
    crates/sui$ cargo build
    sui$ export SUI=$(git rev-parse --show-toplevel)/target/debug/sui
    sui$ $SUI genesis -f && $SUI start
    
         # In a new terminal session
    sui$ export SUI=$(git rev-parse --show-toplevel)/target/debug/sui
    sui$ $SUI client switch --env localnet
    sui$ ./scripts/check-framework-compat.sh
    ```

---
+++ scripts/check-framework-compat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Check whether the version of framework in the repo is compatible
+# with the version on chain, as reported by the currently active
+# environment, using the binary in environment variable $SUI.
+
+set -e
+
+SUI=${SUI:-sui}
+REPO=$(git rev-parse --show-toplevel)
+
+for PACKAGE in "$REPO"/crates/sui-framework/packages/*; do
+    $SUI client verify-source "$PACKAGE"
+done
+
