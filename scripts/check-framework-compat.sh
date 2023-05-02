f53f84efaf23b64b26eb26bac058db022a50666d
Ashok Menon <ashok@mystenlabs.com>
2023-05-01 17:39:24 -0700

    fixup: License

--- scripts/check-framework-compat.sh
+++ scripts/check-framework-compat.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
 # Check whether the version of framework in the repo is compatible
 # with the version on chain, as reported by the currently active
 # environment, using the binary in environment variable $SUI.
